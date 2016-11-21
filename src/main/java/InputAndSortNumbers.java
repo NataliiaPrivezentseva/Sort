030df1e58819ffec670ab0599274983d0c8b39b9
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2016-11-21 11:55:55 +0100

    refactor chooseSortMethod()

--- src/main/java/InputAndSortNumbers.java
+++ src/main/java/InputAndSortNumbers.java
@@ -30,11 +30,10 @@ public class InputAndSortNumbers {
         System.out.println();
     }
 
+//    code without using regex
     private static SortNumbers chooseSortMethod() {
         int choice = 0;
-        String usersChoice;
         Scanner input = new Scanner(System.in);
-        String regex = "[1-8]";
         do {
             System.out.println("Please, choose sortNumberCollection method:\n" +
                     "1 — Bubble Sort\n" +
@@ -46,18 +45,17 @@ public class InputAndSortNumbers {
                     "7 — Merge Sort\n" +
                     "8 — Heap Sort");
 
-            usersChoice = input.next();
-            try {
-                choice = Integer.parseInt(usersChoice);
-            } catch (NumberFormatException e) {
-                System.out.println("\'" + usersChoice + "\' is not proper choice! Try again!");
+            if (!input.hasNextInt()) {
+                String usersInput = input.next();
+                System.out.println("\'" + usersInput + "\' is not proper choice! Try again!");
+            } else {
+                choice = input.nextInt();
+                if (choice <= 0 || choice > 8) {
+                    System.out.println("\'" + choice + "\' is not proper choice! Try again!");
+                }
             }
 
-            if (!usersChoice.matches(regex)) {
-                System.out.println("\'" + usersChoice + "\' is not proper choice! Try again!");
-            }
-
-        } while (!usersChoice.matches(regex));
+        } while (choice <= 0 || choice > 8);
 
         SortNumbers chosenSortMethod;
         switch (choice) {
@@ -90,6 +88,65 @@ public class InputAndSortNumbers {
         }
         return chosenSortMethod;
     }
+
+    // with using regex
+//    private static SortNumbers chooseSortMethod() {
+//        int choice = 0;
+//        String usersChoice;
+//        Scanner input = new Scanner(System.in);
+//        String regex = "[1-8]";
+//        do {
+//            System.out.println("Please, choose sortNumberCollection method:\n" +
+//                    "1 — Bubble Sort\n" +
+//                    "2 — Selection Sort\n" +
+//                    "3 — Insertion Sort\n" +
+//                    "4 — Shell Sort\n" +
+//                    "5 — Quick Sort\n" +
+//                    "6 — Bucket Sort\n" +
+//                    "7 — Merge Sort\n" +
+//                    "8 — Heap Sort");
+//
+//            usersChoice = input.next();
+//
+//            if (!usersChoice.matches(regex)) {
+//                System.out.println("\'" + usersChoice + "\' is not proper choice! Try again!");
+//            } else {
+//                choice = Integer.parseInt(usersChoice);
+//            }
+//
+//        } while (choice <= 0 || choice > 8);
+//
+//        SortNumbers chosenSortMethod;
+//        switch (choice) {
+//            case 1:
+//                chosenSortMethod = new BubbleSort();
+//                break;
+//            case 2:
+//                chosenSortMethod = new SelectionSort();
+//                break;
+//            case 3:
+//                chosenSortMethod = new InsertionSort();
+//                break;
+//            case 4:
+//                chosenSortMethod = new ShellSort();
+//                break;
+//            case 5:
+//                chosenSortMethod = new QuickSort();
+//                break;
+//            case 6:
+//                chosenSortMethod = new BucketSort();
+//                break;
+//            case 7:
+//                chosenSortMethod = new MergeSort();
+//                break;
+//            case 8:
+//                chosenSortMethod = new HeapSort();
+//                break;
+//            default:
+//                throw new InputMismatchException();
+//        }
+//        return chosenSortMethod;
+//    }
 
 }
 
